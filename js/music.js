5052b1ffd43dba4c1d1c6bb3559870b781c34245
Michael Freeman <michael.k.freeman@gmail.com>
2015-10-22 10:51:12 -0700

    Finished activity

--- js/music.js
+++ js/music.js
@@ -19,8 +19,9 @@ $('form').submit(function() {
 	})
 
 	// After setting each property, save your new instance back to your database
-	music.save()
-	
+	music.save(null, {
+		success:getData
+	})
 	return false
 })
 
@@ -31,38 +32,59 @@ var getData = function() {
 	
 
 	// Set up a new query for our Music class
-
+	var query = new Parse.Query(Music)
 
 	// Set a parameter for your query -- where the website property isn't missing
-
+	query.notEqualTo('website', '')
 
 	/* Execute the query using ".find".  When successful:
 	    - Pass the returned data into your buildList function
 	*/
+	query.find({
+		success:function(results) {
+			buildList(results)
+		} 
+	})
 }
 
 // A function to build your list
 var buildList = function(data) {
-	// Empty out your unordered list
-	
-	// Loop through your data, and pass each element to the addItem function
+	// Empty out your ordered list
+	$('ol').empty()
 
+	// Loop through your data, and pass each element to the addItem function
+	data.forEach(function(d){
+		addItem(d);
+	})
 }
 
 
 // This function takes in an item, adds it to the screen
 var addItem = function(item) {
 	// Get parameters (website, band, song) from the data item passed to the function
-
+	var website = item.get('website')
+	var band = item.get('band')
+	var song = item.get('song')
 	
 	// Append li that includes text from the data item
-
-
+	var li = $('<li>Check out ' + band + ', their best song is ' + song + '</li>')
 	
-	// Time pending, create a button that removes the data item on click
+	// Create a button with a <span> element (using bootstrap class to show the X)
+	var button = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>')
+	
+	// Click function on the button to destroy the item, then re-call getData
+	button.click(function() {
+		item.destroy({
+			success:getData
+		})
+	})
+
+	// Append the button to the li, then the li to the ol
+	li.append(button);
+	$('ol').append(li)
 	
 }
 
 // Call your getData function when the page loads
-
+getData()
 
